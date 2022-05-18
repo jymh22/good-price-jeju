7761bd0efa7dd9a3bfd1c6386b829ac2abcf6618
NAMUORI <toolbar@jejunu.ac.kr>
2022-05-18 17:51:26 +0900

    ip 위치기반정보 api 추가

--- app.js
+++ app.js
@@ -17,8 +17,6 @@ navigator.geolocation.getCurrentPosition(function(position) {
     lon = position.coords.longitude; // 경도
 });
 
-console.log(lat, lon);
-
 var container = document.getElementById('map');
 var options = {
     center: new kakao.maps.LatLng(lat, lon),
@@ -26,6 +24,32 @@ var options = {
 };
      
 var map = new kakao.maps.Map(container, options);
+
+////////////////////////////////////////////////////////////////
+if (navigator.geolocation) {
+    
+    // GeoLocation을 이용해서 접속 위치를 얻어옵니다
+    navigator.geolocation.getCurrentPosition(function(position) {
+        
+        var lat = position.coords.latitude, // 위도
+            lon = position.coords.longitude; // 경도
+        
+        var locPosition = new kakao.maps.LatLng(lat, lon), // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
+            message = '<div style="padding:5px;">여기에 계신가요?!</div>'; // 인포윈도우에 표시될 내용입니다
+        
+        // 마커와 인포윈도우를 표시합니다
+        displayMarker(locPosition, message);
+            
+      });
+    
+} else { // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
+    
+    var locPosition = new kakao.maps.LatLng(33.450701, 126.570667),    
+        message = 'geolocation을 사용할수 없어요..'
+        
+    displayMarker(locPosition, message);
+}
+////////////////////////////////////////////////////////////////
 
 // 주소-좌표 변환 객체를 생성합니다
 var geocoder = new kakao.maps.services.Geocoder();
@@ -55,6 +79,33 @@ const searchMap = (address) => {
             map.setCenter(coords);
         } 
     });
+}
+
+const display_geolocation = () => {
+        // HTML5의 geolocation으로 사용할 수 있는지 확인합니다 
+    if (navigator.geolocation) {
+        
+        // GeoLocation을 이용해서 접속 위치를 얻어옵니다
+        navigator.geolocation.getCurrentPosition(function(position) {
+            
+            var lat = position.coords.latitude, // 위도
+                lon = position.coords.longitude; // 경도
+            
+            var locPosition = new kakao.maps.LatLng(lat, lon), // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
+                message = '<div style="padding:5px;">여기에 계신가요?!</div>'; // 인포윈도우에 표시될 내용입니다
+            
+            // 마커와 인포윈도우를 표시합니다
+            displayMarker(locPosition, message);
+                
+        });
+        
+    } else { // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
+        
+        var locPosition = new kakao.maps.LatLng(33.450701, 126.570667),    
+            message = 'geolocation을 사용할수 없어요..'
+            
+        displayMarker(locPosition, message);
+    }
 }
 
 
@@ -145,18 +196,22 @@ searchBtn.addEventListener('click', () => {
 sortBox.addEventListener('change', (event) => {
     if (event.target.value === 'food') {
         contentsList.innerHTML = addList(food);
+        sortCategory();
         addressFunction();
     }
     else if (event.target.value === 'hair') {
         contentsList.innerHTML = addList(hair);
+        sortCategory();
         addressFunction();
     }
     else if (event.target.value === 'laundry') {
         contentsList.innerHTML = addList(laundry);
+        sortCategory();
         addressFunction();
     }
     else if (event.target.value === 'stay') {
         contentsList.innerHTML = addList(stay);
+        sortCategory();
         addressFunction();
     }
 });
@@ -172,5 +227,4 @@ searchBar.addEventListener('keydown', (event) => {
 window.addEventListener('load', () => {
     contentsList.innerHTML = addList(contents);
     addressFunction();
-    sortCategory();
 });
