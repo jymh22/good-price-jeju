e7dead6f939a8934b80679b48c6e938e4f0e1736
jymh22 <jymh123@gmail.com>
2022-06-17 02:39:53 +0900

    Update app.js

--- app.js
+++ app.js
@@ -190,16 +190,10 @@ const addList = (listObj) => {
     //var a, b, c, d;
 
     for (let i = 0; i < listObj.length; i++) {
-        //fetch("https://maps.googleapis.com/maps/api/place/textsearch/json?query=삼매봉153 제주특별자치도 서귀포시 남성중로 153번길 15&key=AIzaSyAaMQd2lgwFeocbFvUpt99vJFyGVPa0g9o").then((response) =>  response.json() );
-        //b = a.results[0].place_id;
-        //fetch("https://maps.googleapis.com/maps/api/place/details/json?fields=name%2Cphotos%2Crating&place_id=" + b + "&key=AIzaSyAaMQd2lgwFeocbFvUpt99vJFyGVPa0g9o").then((response) =>  c );
-        //b = a.results[0].rating;
-        //c = a.results[0].photos[0].photo_reference;
-        //fetch("https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=" + c + "&key=AIzaSyAaMQd2lgwFeocbFvUpt99vJFyGVPa0g9o").then((response) =>  d );
-        var imgscr = getrating(listObj[i].title, listObj[i].address);
-        var rating = getrating(listObj[i].title, listObj[i].address);
+        //var imgscr = getrating(listObj[i].title, listObj[i].address);
+        //var rating = getrating(listObj[i].title, listObj[i].address);
         list += `<div class='list'>
-                <img src=${imgscr}>
+                <img src=${listObj[i].img}>
                 <span>
                     <ul>
                         <li id='title'>업소명 : ${listObj[i].title}</li>
@@ -207,7 +201,6 @@ const addList = (listObj) => {
                         <li>연락처 : ${listObj[i].phone}</li>
                         <li>품목 : ${listObj[i].menu}</li>
                         <li id='address'>주소 : ${listObj[i].address}</li>
-                        <li>별점 : ${rating}</li>
                     </ul>
                 </span>
         </div>`;
